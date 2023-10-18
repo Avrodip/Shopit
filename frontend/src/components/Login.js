051197df3508d55d2fe6cae70038410f8f85b76b
Avrodip <avrodp25@gmail.com>
2023-10-18 16:18:23 +0530

    First Message

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <form>
+        <h3>Signin</h3>
+      </form>
+    </div>
+  )
+}
+
+export default Login
