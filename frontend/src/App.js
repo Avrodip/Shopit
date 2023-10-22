33f36221d1f58cf742e3d02466d036279557c84f
Avrodip <avrodp25@gmail.com>
2023-10-22 15:09:02 +0530

    Signup and Login addedd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Login from './components/Login';
 import Signup from './components/Signup';
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,53 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import './LoginValidation';
 const Login = () => {
-  return (
-    <div className='login'>
-      <form>
-        <h3>Signin</h3>
-      </form>
-    </div>
-  )
+  const[values,setValues]=useState({
+    email: '',
+    password: ''
+  })
+
+const handleSubmit=(event)=>{
+  event.preventDefault();
 }
 
-export default Login
+const handleInput=(event)=>{
+  setValues(prev=>({...prev,[event.target.value]:[event.target.value]}))  
+}
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <form action="" onSubmit={handleSubmit}>
+          <h2 className="text-center mb-4">Sign In</h2>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              onChange={handleInput}
+              className="form-control rounded-0"
+              name="email"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label" >
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              onChange={handleInput}
+              className="form-control rounded-0"
+              name="password"
+            />
+          </div>
+          <button className="btn btn-success w-100 mb-2" type="submit">Log in</button>
+          <button className="btn btn-default border w-100 bg-light">Create Account</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
