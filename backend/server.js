cc80859c8f1a468428914265e78a0bf9edbc6a7f
Avrodip <avrodp25@gmail.com>
2023-10-25 12:00:42 +0530

    Login Validation fix

--- backend/server.js
+++ backend/server.js
@@ -14,48 +14,104 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/admin', async (req, res) => {
-    try {
-      const connection = await mysql.createConnection(dbConfig);
-  
-      const [rows] = await connection.execute("Select * from admins");
-  
-      connection.end();
-      res.status(200).json(rows);
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
-  app.get('/suppliers', async (req, res) => {
-    try {
-      const connection = await mysql.createConnection(dbConfig);
-  
-      const [rows] = await connection.execute("Select * from suppliers");
-  
-      connection.end();
-      res.status(200).json(rows);
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  try {
+    const connection = await mysql.createConnection(dbConfig);
 
-  app.get('/users', async (req, res) => {
-    try {
-      const connection = await mysql.createConnection(dbConfig);
-  
-      const [rows] = await connection.execute("Select * from users");
-  
-      connection.end();
-      res.status(200).json(rows);
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    const [rows] = await connection.execute("Select * from admins");
+    
+    connection.end();
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/suppliers', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const [rows] = await connection.execute("Select * from suppliers");
+
+    connection.end();
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const [rows] = await connection.execute("Select * from users");
+
+    connection.end();
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    
+    const connection = await mysql.createConnection(dbConfig);
+    const tableName = (role === 'admin') ? 'admins' : (role === 'supplier') ? 'suppliers' : 'users';
+
+    const [rows] = await connection.execute(`SELECT * FROM ${tableName} WHERE email = ? AND password = ?`, [email, password]);
+
+    connection.end();
+    
+    if (rows.length === 1) {
+      res.status(200).json({ message: 'Login successful' });
+    } else {
+      res.status(401).json({ error: 'Invalid credentials' });
     }
-  });
-  const port = 3002;
-  app.listen(port, () => {
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.post('/signup', async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      address,
+      phoneNumber,
+      email,
+      username,
+      password,
+      role,
+      position,
+      hireDate,
+    } = req.body;
+
+    const connection = await mysql.createConnection(dbConfig);
+
+    if(role==="admin"){
+    await connection.execute(
+      `INSERT INTO admins (first_name, last_name, position, hire_date, phone, address)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [firstName, lastName, address, phoneNumber, email, username, password, position, hireDate]
+    );
+    }
+
+    connection.end();
+    res.status(200).json({ message: 'Registration successful' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+const port = 3002;
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
-  
