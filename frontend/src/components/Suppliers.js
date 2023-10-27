d14c950053dc7465c753100ed11a1a3ffb20a1a8
Vandana Ranjan <85933277+vandana-techno@users.noreply.github.com>
2023-10-27 14:59:39 +0530

    Update Suppliers.js

--- frontend/src/components/Suppliers.js
+++ frontend/src/components/Suppliers.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 import axios from "axios";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Suppliers = () => {
