1ea6cf9640814970a4891a5a092cecfd45e6af53
Vandana Ranjan <85933277+vandana-techno@users.noreply.github.com>
2023-10-27 15:22:04 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link, Navigate } from 'react-router-dom';
+import './Home.css';
 
 const Home = ({ loggedIn,logindet }) => {
  
@@ -17,19 +18,27 @@ const Home = ({ loggedIn,logindet }) => {
     return <Navigate to="/confirmedorders" />;
   }
 
-  
 
-  return (
+return (
     <div className='btn-mm'>
       <h1>ADMIN LOGIN</h1>
       <div className='btn'>
-        <Link to="/products" className='btn-m'>Product</Link>
-        <Link to="/suppliers" className='btn-m'>Supplier</Link>
-        <Link to="/orderconfirm" className='btn-m'>Order</Link> 
-        <Link to="/customers" className='btn-m'>Customer</Link>
+        <Link to="/products">
+          <button className='btn-m'>Product</button>
+        </Link>
+        <Link to="/Suppliers">
+          <button className='btn-m'>Supplier</button>
+        </Link>
+        <Link to="/order">
+          <button className='btn-m'>Order</button>
+        </Link>
+        <Link to="/customer">
+          <button className='btn-m'>Customer</button>
+        </Link>
       </div>
     </div>
-  );
+  )
 }
+
 
 export default Home;
