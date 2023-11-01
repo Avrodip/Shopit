8339dd89ceb95eb257e2de40ec21d8cbd9053beb
Garvit Sharma <60612387+gar-git@users.noreply.github.com>
2023-11-01 13:19:49 +0530

    Update Display.js

--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -2,15 +2,18 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 
-const Display = () => {
-  console.log("Diplay called");
+const Display = (props) => {
   const [items, setItems] = useState([]);
+  const { id } = useParams();
+  const [userID, setUserID] = useState(0);
+  const uname = props.logindet.username;
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    console.log("useeffect called");
     async function fetchItems() {
-      console.log("item fetched called");
       try {
         const res = await axios.get("http://localhost:3002/products");
         setItems(res.data);
@@ -18,16 +21,41 @@ const Display = () => {
         console.log(err);
       }
     }
-    fetchItems();
-  }, []);
 
-  console.log(items);
+    async function fetchID() {
+      try {
+        console.log("user name is: ", uname);
+        const response = await axios.get(`http://localhost:3002/users`, {
+          params: {
+            Username: uname,
+          },
+        });
+        setUserID(response.data[0].User_id);
+        console.log(response.data[0].User_id);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchItems();
+    fetchID();
+  }, []);
 
   return (
     <div>
+      <button
+        className="btn btn-secondary"
+        onClick={() => {
+          navigate(`/viewCart/${userID}`);
+        }}
+        style={{ background: "#385a64", marginTop: "10px" }}
+      >
+        View Cart items
+      </button>
       <div
         className="items"
         style={{
+          backgroundColor: "#fff",
           display: "flex",
           marginTop: "90px",
           justifyContent: "center",
@@ -43,13 +71,13 @@ const Display = () => {
               src={item.imglink1}
               className="img-fluid"
               alt="image unavailable"
-              style={{ width: "200px", height: "200px" }}
+              style={{ width: "200px", height: "200px", borderRadius: "50%" }}
             />
             <h4>{item.Product_name}</h4>
             <p>{item.Description}</p>
             <span>Rs.{item.Sell_price}</span>
             <br />
-            <button className="addCart">Add to cart</button>
+            <button className="btn btn-secondary addCart">Add to cart</button>
           </div>
         ))}
       </div>
