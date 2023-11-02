c6a4ce4f9f3efea9b8ca65c531afbb4406f16ed4
Garvit Sharma <60612387+gar-git@users.noreply.github.com>
2023-11-02 19:38:26 +0530

    EmptyCart.js updated with image

--- frontend/src/components/EmptyCart.js
+++ frontend/src/components/EmptyCart.js
@@ -3,11 +3,12 @@ import { useNavigate } from "react-router-dom";
 function EmptyCart() {
   const navigate = useNavigate();
   return (
-    <div
-      className="d-flex vh-100"
-      style={{ justifyContent: "center", alignItems: "center" }}
-    >
-      <div className="  bg-white p-3  rounded w-35 border border-dark rounded p-4">
+    <div className="d-flex vh-100 justify-content-center align-items-center">
+      <div
+        className="  bg-white p-3  rounded w-35 border border-dark rounded justify-content-center align-items-center p-4"
+        style={{ justifyContent: "center", alignItems: "center" }}
+      >
+        <img src="https://github.com/gar-git/images/blob/main/Screenshot_2023-11-02_185431-removebg-preview.png?raw=true"></img>
         <h1>OOPS!! The cart is empty</h1>
         <button
           style={{ justifyContent: "center", alignItems: "center" }}
@@ -16,7 +17,7 @@ function EmptyCart() {
             navigate("/displayproduct");
           }}
         >
-          Return to home
+          ← Return to home
         </button>
       </div>
     </div>
