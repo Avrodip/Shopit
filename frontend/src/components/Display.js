741479312d781a351be26ff52e2ea48e6e5f3081
Garvit Sharma <60612387+gar-git@users.noreply.github.com>
2023-11-02 19:35:26 +0530

    Display.js color changed

--- frontend/src/components/Display.js
+++ frontend/src/components/Display.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 
+import "./cart.css";
 const Display = (props) => {
   const [items, setItems] = useState([]);
   const { id } = useParams();
@@ -32,6 +33,10 @@ const Display = (props) => {
         });
         setUserID(response.data[0].User_id);
         console.log(response.data[0].User_id);
+        // console.log("helooooooooo", response.data[0].User_id);
+        // const data = await response.data;
+        // setUserID(data);
+        // console.log("user id is: ", userID);
       } catch (err) {
         console.log(err);
       }
@@ -43,41 +48,61 @@ const Display = (props) => {
 
   return (
     <div>
-      <button
-        className="btn btn-secondary"
+      <img
+        src="https://github.com/gar-git/images/blob/main/cart-icon-28344.png?raw=true"
+        className="lo"
+        id="btn-1"
+        width="60px"
+        height="60px"
+        alt="Logo"
+        style={{
+          padding: "5px",
+          marginLeft: "95%",
+          marginTop: "5px",
+          borderRadius: "50%",
+          position: "fixed",
+        }}
         onClick={() => {
           navigate(`/viewCart/${userID}`);
         }}
-        style={{ background: "#385a64", marginTop: "10px" }}
-      >
-        View Cart items
-      </button>
+      />
       <div
-        className="items"
-        style={{
-          backgroundColor: "#fff",
-          display: "flex",
-          marginTop: "90px",
-          justifyContent: "center",
-        }}
+        className="items d-flex flex-wrap w-90 justify-content-center m-auto"
+        id="bg"
       >
         {items.map((item) => (
           <div
             key={item.Product_id}
             className="item"
-            style={{ padding: "10px" }}
+            style={{
+              padding: "10px",
+              border: "2px solid",
+              borderRadius: "9%",
+              margin: "20px",
+              background: "#ebe6e2",
+              width: "300px",
+            }}
           >
-            <img
-              src={item.imglink1}
-              className="img-fluid"
-              alt="image unavailable"
-              style={{ width: "200px", height: "200px", borderRadius: "50%" }}
-            />
-            <h4>{item.Product_name}</h4>
-            <p>{item.Description}</p>
-            <span>Rs.{item.Sell_price}</span>
-            <br />
-            <button className="btn btn-secondary addCart">Add to cart</button>
+            <center>
+              <img
+                src={item.imglink1}
+                className="img-fluid"
+                alt="image unavailable"
+                style={{
+                  width: "200px",
+                  height: "200px",
+                  borderRadius: "50%",
+                  border: "2px solid",
+                }}
+              />
+              <h4>{item.Product_name}</h4>
+              <p style={{ height: "70px" }}>{item.Description}</p>
+              <span>Rs.{item.Sell_price}</span>
+              <br />
+              <button className="btn btn-secondary addCart" id="btn-1">
+                Add to cart
+              </button>
+            </center>
           </div>
         ))}
       </div>
